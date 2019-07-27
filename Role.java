067d36ef21f6ceb2d0a9380ce3d9357eaa6023ac
GLliangcheng2 <351070395@qq.com>
2019-07-27 16:46:46 +0800

    tij

--- Role.java
+++ Role.java
@@ -2,8 +2,8 @@ public class Role{
 	public static void main(String[] args){		
 
 		System.out.print("hello word7777!");
-		
-
-		
+		System.out.print("hello word7777!");
+		System.out.print("hello word7777!");
+		System.out.print("hello word7777!");
 	}
 }
