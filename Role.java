e2487b48c92d32b388540da683fefe20ae2bf1e2
GLliangcheng <17621813115@163.com>
2019-07-27 16:43:49 +0800

    提交1

--- Role.java
+++ Role.java
@@ -2,6 +2,10 @@ public class Role{
 	public static void main(String[] args){		
 		System.out.print("hello word8888!");
 		System.out.print("hello word8888!");
+		System.out.print("hello word8888!");
+		System.out.print("hello word8888!");
+		System.out.print("hello word8888!");
+		System.out.print("hello word8888!");
 		
 	}
 }
